bc1190c75e258d81b0266547dfedea05bac480fc
adebolaaaaaaaaa <ayeniadebola444@gmail.com>
2022-01-30 21:15:55 +0100

    updated code

--- randomcolorgenerator/script.js
+++ randomcolorgenerator/script.js
@@ -1,11 +1,12 @@
 function randomNum(){
-	hex = Math.floor(Math.random() * 12);
+	return(Math.floor(Math.random() * 16));
 }
 function change(){
 	var all = ["A", "B", "C", "D", "E", "F", 0,1,2,3,4,5,6,7,8,9];
-	var hexCode = "#" + all[randomNum(), hex]+ all[randomNum(), hex]+ all[randomNum(), hex]+ all[randomNum(), hex]+ all[randomNum(), hex]+ all[randomNum(), hex] ;
+	var hexCode = "#" + all[randomNum()]+ all[randomNum()]+ all[randomNum()]+ all[randomNum()]+ all[randomNum()]+ all[randomNum()] ;
     document.body.style.backgroundColor = hexCode;
     document.getElementById('hexcode').innerHTML = hexCode;
+	document.getElementById("rgb").innerHTML = document.body.style.backgroundColor;
 	console.log(hexCode);
 }
 window.onload = change();
